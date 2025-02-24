014a318989b1c982d6d77a62ceb81cc2c13df132
Saulbez <148885604+Saulbez@users.noreply.github.com>
2025-02-24 19:24:22 -0300

    Login page designed and better responsiveness
    - Improvements on responsiveness
    - Login page designed

--- Server.js
+++ Server.js
@@ -8,13 +8,35 @@ const port = 3000;
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
+let users = [
+    ["saul", "123"]
+]
+
 app.get("/", async (req, res) => {
     res.render("index.ejs", { title: "Home" });
 })
 
 app.get("/clientes", async (req, res) => {
-    res.render("clients.ejs", { title: "Home" });
+    res.render("clients.ejs", { title: "Clientes" });
 })
+
+app.get("/produtos", async (req, res) => {
+    res.render("products.ejs", { title: "Produtos" });
+})
+
+app.get("/login", async (req, res) => {
+    res.render("login.ejs", { title: "Login" });
+})
+
+app.post("/login", async (req, res) => {
+    const username = req.body["username"];
+    const password = req.body["password"];
+
+    if (users[0][0] === username && users[0][1] === password) {
+        res.render("index.ejs", { title: "Home" });
+    }
+
+});
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
